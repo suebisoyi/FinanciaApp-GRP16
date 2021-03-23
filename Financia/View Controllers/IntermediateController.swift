c597113beb8e382797501fc9e66b0a841cdb2dea
Harman Bath <harman.bath@outlook.com>
2021-03-23 00:28:35 -0400

    Added "intermediate" and "advanced" scenes for UI for navigation. Added "Payment Options" scene for UI with navigation.

---
+++ Financia/View Controllers/IntermediateController.swift
@@ -0,0 +1,29 @@
+//
+//  IntermediateController.swift
+//  Financia
+//
+//  Created by Harman Bath on 2021-03-23.
+//
+
+import UIKit
+
+class IntermediateController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Financia/View Controllers/PaymentOptionsController.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentOptionsController.swift
+//  Financia
+//
+//  Created by Harman Bath on 2021-03-23.
+//
+
+import UIKit
+
+class PaymentOptionsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
