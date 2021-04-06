b814bb5b973c0c7bf1edcba87ff0e3077fc7f457
Gagandeep Ghotra <ghotraga@sheridancollege.ca>
2021-04-06 07:43:13 -0700

    Made a new login view controller

--- Financia/View Controllers/ViewController.swift
+++ Financia/View Controllers/ViewController.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-//import Auth0
-// Auth0 Module not found?
 
 class ViewController: UIViewController {
 
@@ -16,29 +14,5 @@ class ViewController: UIViewController {
         // Do any/Volumes/Student/FinanciaApp-GRP16-GG/Financia/Storyboards/Base.lproj/Main.storyboard additional setup after loading the view.
         
     }
-    
-    /*Auth0
-        .webAuth()
-        .scope("openid profile")
-        .audience("https://dev-y8kzi2o4.us.auth0.com/userinfo")
-        .start { result in
-            switch result {
-            case .failure(let error):
-                // Handle the error
-                print("Error: \(error)")
-            case .success(let credentials):
-                // Do something with credentials e.g.: save them.
-                // Auth0 will automatically dismiss the login page
-                print("Credentials: \(credentials)")
-            }
-    }
-    
-    Auth0
-        .webAuth()
-        .clearSession(federated: false) { result in
-            if result {
-                // Session cleared
-            }
-        }}*/
 
 }
