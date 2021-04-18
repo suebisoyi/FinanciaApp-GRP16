d44098f135da58baabd056735e7fcf729ef84c67
abdikasa <abdikasa@sheridancollege.ca>
2021-04-18 13:41:25 -0700

    Added base line chart, will use bar chart to show the data from the DB.

--- Financia/View Controllers/ShowChartViewController.swift
+++ Financia/View Controllers/ShowChartViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 import Charts
 
-class ShowChartViewController: UIViewController {
+class ShowChartViewController: UIViewController, ChartViewDelegate {
+    
     private var viewModel = StatementViewModel()
     let mainDelegate = UIApplication.shared.delegate as! AppDelegate
     @IBOutlet weak var closeButton: UIButton!
     @IBOutlet weak var pieView: PieChartView!
     let statementCat = ["Expenses", "Income"]
     var statementValues : [Int] = []
- 
-    
+    @IBOutlet weak var lineChart: BarChartView!
+
     override func viewDidLoad() {
       super.viewDidLoad()
         pieView.legend.font = UIFont.systemFont(ofSize: 15.0)
@@ -25,6 +26,44 @@ class ShowChartViewController: UIViewController {
         let incomeExpense = mainDelegate.sumExpensesIncome()
         statementValues = incomeExpense
         customizeChart(dataPoints: statementCat, values: statementValues.map{ Double($0) })
+        lineChart.delegate = self
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+        //view.addSubview(lineChart)
+        
+        var entries = [BarChartDataEntry]()
+        
+        var lines = ["a", "b", "c"]
+        
+        for x in 0..<3 {
+            var barchentry = BarChartDataEntry(x: Double(x), y: Double(5))
+            entries.append(barchentry)
+        }
+    
+        let set = BarChartDataSet(entries: entries, label: "Top 3 expenses from largest to smallest")
+        set.colors = ChartColorTemplates.material()
+        
+        let data = BarChartData(dataSet: set)
+        data.barWidth = 0.2
+        
+        lineChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: lines)
+        lineChart.xAxis.labelPosition = .top
+        lineChart.xAxis.granularity = 1
+        lineChart.rightAxis.enabled = false
+        lineChart.zoom(scaleX: 1.5, scaleY: 1, x: 0, y: 0)
+        lineChart.setVisibleXRangeMinimum(10)
+        lineChart.setVisibleXRangeMaximum(10)
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .none
+        lineChart.leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: formatter)
+        lineChart.leftAxis.axisMinimum = 0
+        lineChart.data = data
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
